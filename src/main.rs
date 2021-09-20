015d17d1f63680de4b2e42c489e08f71d8b7c019
hodongman <jhd9206@gmail.com>
2021-09-21 02:48:04 +0900

    db connection update

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 
 mod handlers;
 
-
+#[macro_use]
 extern crate diesel;
+
 extern crate r2d2;
 extern crate r2d2_diesel;
 
 use actix_web::{dev::ServiceRequest, web, App, Error, HttpServer};
 use diesel::prelude::*;
+
 use r2d2_diesel::ConnectionManager;
+use diesel::mysql::MysqlConnection;
 
 use dotenv::dotenv;
 
@@ -20,8 +23,14 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     std::env::set_var("RUST_LOG", "actix_web=debug");
 
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let manager = ConnectionManager::<MysqlConnection>::new(database_url);
+    let db_pool: Pool = r2d2::Pool::builder().build(manager).expect("Failed to create pool.");
+
     HttpServer::new(move || {
         App::new()
+            .data(db_pool.clone())
             .route("/users", web::get().to(handlers::get_users))
             .route("/users/{id}", web::get().to(handlers::get_user_by_id))
             .route("/users", web::post().to(handlers::add_user))
